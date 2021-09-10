5ce04694e5e449c6b17931ddc6c0a508e0ca1037
xxierr <90265539+xxierr@users.noreply.github.com>
2021-09-10 18:46:32 +0800

    Add files via upload

---
+++ test_test.py
@@ -0,0 +1,30 @@
+import pytest
+import time
+
+
+class Testtest():
+
+    @pytest.fixture(scope='function')
+    def test_inout(self):
+        print('成功打开浏览器并进入禅道')
+        yield 1
+        time.sleep(2)
+        print('退出成功！')
+        time.sleep(5)
+        print('成功关闭浏览器！')
+
+    def test_login1(self, test_inout):
+        assert test_inout() == 1
+            print("登录成功")
+        except AssertionError:
+            print("登录失败")
+
+    def test_login2(self,test_inout):
+        assert test_inout() == 1
+            print('测试用例执行成功，验证错误的密码')
+        except AssertionError:
+            print('测试用例执行失败')
+
+
+if __name__ == '__main__':
+    pytest.main(['-vs', 'test_test.py'])
